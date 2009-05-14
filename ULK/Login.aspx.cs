7353b239c8a23f5b83ec0c565d7f0cff5fea17d0
for.boss <for.boss@gmail.com>
2009-05-14 21:35:09 +0000

    Design update

--- ULK/Login.aspx.cs
+++ ULK/Login.aspx.cs
@@ -9,15 +9,6 @@ public partial class Login : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
-    }
-    protected void RememberMe_CheckedChanged(object sender, EventArgs e)
-    {
-
-    }
-
-    protected void redirectToHomePage_Click(object sender, EventArgs e)
-    {
         if (User.IsInRole(Constants.AdminRoleName))
         {
             Response.Redirect(UrlConstants.AdminHomeUrl);
@@ -30,6 +21,16 @@ public partial class Login : System.Web.UI.Page
         {
             Response.Redirect(UrlConstants.UserHomeUrl);
         }
+
+    }
+    protected void RememberMe_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void redirectToHomePage_Click(object sender, EventArgs e)
+    {
+
     }
     protected void loginView_ViewChanged(object sender, EventArgs e)
     {
