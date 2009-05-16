7dcaec6715e561e1321cfe33365ccae058ed84de
peter.kunilov@gmail.com <peter.kunilov@gmail.com@dc05cc5a-cc87-11dd-a412-a1792178251d>
2009-05-16 15:38:31 +0000

    Сделано назначение тестов студентам

--- trunk/ULK/User/UserMasterPage.master.cs
+++ trunk/ULK/User/UserMasterPage.master.cs
@@ -9,6 +9,6 @@ public partial class User_UserMasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        UsersRules.IsUser(Page.User, Response);
     }
 }
