42eaac32eb35f89f8aca9d6042779dff46d3d4be
peter.kunilov@gmail.com <peter.kunilov@gmail.com@dc05cc5a-cc87-11dd-a412-a1792178251d>
2009-11-19 21:06:17 +0000

    Добавлен новый вариант локальной БД. Все старые варианты перемещены в папку DB_OLD

--- trunk/ULK/UnnLearningKitLibrary/Department.cs
+++ trunk/ULK/UnnLearningKitLibrary/Department.cs
@@ -5,31 +5,31 @@ using System.Text;
 
 namespace UnnLearningKitLibrary
 {
-    public class Department : Obj
+public class Department : Obj
+{
+    protected string _title = String.Empty;
+
+    public Department(int id, string title)
+        : base(id)
     {
-        protected string _title = String.Empty;
-
-        public Department(int id, string title)
-            : base(id)
-        {
-            _title = title;
-        }
-
-        public Department(Department department)
-            : base(department.Id)
-        {
-            _title = department.Title;
-        }
-
-        public string Title
-        {
-            get { return _title; }
-            set { _title = value; }
-        }
-
-        public override string ToString()
-        {
-            return _title;
-        }
+        _title = title;
     }
+
+    public Department(Department department)
+        : base(department.Id)
+    {
+        _title = department.Title;
+    }
+
+    public string Title
+    {
+        get { return _title; }
+        set { _title = value; }
+    }
+
+    public override string ToString()
+    {
+        return _title;
+    }
+}
 }
