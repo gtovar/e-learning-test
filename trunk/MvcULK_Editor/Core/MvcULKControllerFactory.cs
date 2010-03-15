24b42007c44c08f1f4e9f325cea245a6ffd840a9
moshkov.sergey <moshkov.sergey@dc05cc5a-cc87-11dd-a412-a1792178251d>
2010-03-15 20:05:22 +0000

    Редактор тестовых вопросов

---
+++ trunk/MvcULK_Editor/Core/MvcULKControllerFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using MvcULK_Editor.Models;
+
+namespace MvcULK_Editor.Core
+{
+    public class MvcULKControllerFactory : DefaultControllerFactory
+    {
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ULKDBConnectionString"].ConnectionString;
+
+            return Activator.CreateInstance(controllerType, new RepositoryManager(connectionString)) as IController;
+        }
+    }
+}
